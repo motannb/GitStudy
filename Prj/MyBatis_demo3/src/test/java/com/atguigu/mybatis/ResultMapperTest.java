7e60f7a7f87cc5a310563cbea8fc488fe85ababf
motannb <244611093@csu.edu.cn>
2025-09-19 00:59:04 +0800

    0918-44/66-分布查询

---
+++ Prj/MyBatis_demo3/src/test/java/com/atguigu/mybatis/ResultMapperTest.java
@@ -0,0 +1,47 @@
+package com.atguigu.mybatis;
+
+import com.atguigu.mybatis.mapper.EmpMapper;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ResultMapperTest {
+
+    /**
+     * 解决字段名和属性名不一致的情况
+     * a>为字段起别名，保持和属性名一致
+     * b>设置全局配置，将下划线自动映射为驼峰
+     * c>通过resultMap自定义映射关系
+     *
+     * 处理多对一的映射关系
+     * a>级联属性赋值
+     * b>association
+     * c>分步查询
+     */
+    @Test
+    public void testGetEmpAndDeptByStep(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpAndDeptByStepOne(1);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testGetEmpAndDept(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpAndDept(3);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testGetAllEmp(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        List<Emp> list = mapper.getAllEmp();
+        list.forEach(emp -> System.out.println(emp));
+    }
+}
